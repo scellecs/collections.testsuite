a4a9777534177905bb828ecf21f33d8a2f34c915
Oleg Morozov <benjminmoore@gmail.com>
2021-01-11 02:49:52 +0300

    init

---
+++ MainFixture.cs
@@ -0,0 +1,25 @@
+﻿namespace Scellecs.Collections.TestSuite {
+    using System.Collections;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using UnityEngine;
+    using UnityEngine.TestTools;
+    using System.Linq;
+
+    public class MainFixture {
+        [Test]
+        [Category("IntHashSet")]
+        public void IntHashSet_Simple_Ctor() {
+            Assert.DoesNotThrow(() => {
+                var ihs = new IntHashSet();
+            });
+        }
+        
+        [Test]
+        [Category("IntHashSet")]
+        public void IntHashSet_Simple_Ctor_With_Capacity() {
+            var ihs = new IntHashSet(30);
+            Assert.AreEqual(64, ihs.capacity);
+        }
+    }
+}
